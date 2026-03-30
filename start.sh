525b25e24b1e5c540206c96280a50686ca15bba2
thenapaextra <nbhi763@gmail.com>
2026-03-30 03:39:25 +0000

    delete

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-echo "Starting second Python script..."
-python migrator.py & python main.py
-# If you want processes to run concurrently, use '&'
-# python script2.py &
-# wait -n # wait for any background process to exit
